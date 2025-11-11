a71e517384222465fa6c4c99bb2c161724156a8c
Gamesrack565 <angelhac17@gmail.com>
2025-11-11 13:16:59 -0600

    Version 0 - Inicio del frontend con Node.js y React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import RegisterPage from './pages/Pagina_Registro/pagina_registro'; // <-- ¡Importación más limpia!
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterPage />
+    </div>
+  );
+}
+
+export default App;
